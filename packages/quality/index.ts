f59ea2f74ce235c44816fd8709c238db932084d8
josuechavando350-png <josuechavando350@gmail.com>
2026-08-16 23:18:17 -0600

    feat(quality): export opening generator

--- packages/quality/index.ts
+++ packages/quality/index.ts
@@ -1,6 +1,7 @@
 export * from "./red-team";
 export * from "./repair-loop";
 export * from "./visual-judge";
+export * from "./opening-generator";
 export * from "./opening-tournament";
 export * from "./mutation-evaluator";
 export * from "./quality-cycle";
