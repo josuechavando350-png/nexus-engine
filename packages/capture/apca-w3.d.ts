45014770f696cb697bef1882bc0a13843d108ea6
josuechavando350-png <josuechavando350@gmail.com>
2026-08-30 15:07:47 -0600

    feat(apca): type APCA font guidance lookup

--- packages/capture/apca-w3.d.ts
+++ packages/capture/apca-w3.d.ts
@@ -1,3 +1,4 @@
 declare module "apca-w3" {
   export function calcAPCA(textColor: string | number | readonly number[], backgroundColor: string | number | readonly number[], places?: number, isInt?: boolean): number | string;
+  export function fontLookupAPCA(contrast: number, places?: number): readonly (number | string)[];
 }
