97e2691fde133bae435b126474d397b8d39eb2db
josuechavando350-png <josuechavando350@gmail.com>
2026-08-29 20:09:11 -0600

    feat: export contextual bandit capability

---
+++ packages/ontology/chatbot-bandit.ts
@@ -0,0 +1,6 @@
+export * from "./chatbot-bandit-types.js";
+export * from "./chatbot-bandit-policy.js";
+export * from "./chatbot-bandit-codec.js";
+export * from "./chatbot-bandit-schema.js";
+export * from "./chatbot-bandit-engine.js";
+export * from "./chatbot-bandit-guardrail.js";
