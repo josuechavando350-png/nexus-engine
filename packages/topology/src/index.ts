5d7340311529b0e3e20dfe7316334c0f8195cd0f
josuechavando350-png <josuechavando350@gmail.com>
2026-08-29 22:39:05 -0600

    audit(topology): export hardened verification boundaries

--- packages/topology/src/index.ts
+++ packages/topology/src/index.ts
@@ -1,11 +1,45 @@
 export type {
-  BottleneckDimensionResult, BottleneckDistanceResult, BuildComplexInput, CertifiedSynthesisInput, CertifiedSynthesisResult,
-  CertifiedSynthesisStatus, FiltrationComplex, FiltrationVertex, FilteredSimplex, PersistenceDiagram, PersistenceDimensionSummary,
-  PersistenceInterval, SimplexDimension, TopologicalFingerprint, TopologicalRelation, TopologyCertificate, TopologyConstraint,
-  TopologyConstraintEvaluation, TopologyConstraintKind, TopologyConstraintSeverity, TopologyConstraintStatus, TopologyPoint, TopologyReference,
+  BottleneckDimensionResult,
+  BottleneckDistanceResult,
+  BuildComplexInput,
+  CertifiedSynthesisInput,
+  CertifiedSynthesisResult,
+  CertifiedSynthesisStatus,
+  FiltrationComplex,
+  FiltrationVertex,
+  FilteredSimplex,
+  PersistenceDiagram,
+  PersistenceDimensionSummary,
+  PersistenceInterval,
+  SimplexDimension,
+  TopologicalFingerprint,
+  TopologicalRelation,
+  TopologyCertificate,
+  TopologyConstraint,
+  TopologyConstraintEvaluation,
+  TopologyConstraintKind,
+  TopologyConstraintSeverity,
+  TopologyConstraintStatus,
+  TopologyPoint,
+  TopologyReference,
 } from "./types.js";
-export { buildComplexFromTerm, buildFiltrationComplex, buildVietorisRipsComplex, simplexId } from "./complex.js";
+export {
+  buildComplexFromTerm,
+  buildFiltrationComplex,
+  buildVietorisRipsComplex,
+  simplexId,
+  validateCanonicalFiltrationComplex,
+} from "./complex.js";
 export { computePersistentHomology, validateFiltrationComplex, validatePersistenceDiagram } from "./homology.js";
 export { bottleneckDistance } from "./distance.js";
-export { compareTopologicalFingerprints, createTopologicalFingerprint } from "./fingerprint.js";
-export { synthesizeCertified, synthesizeTermCertified, validateCertifiedSynthesisResult } from "./synthesis.js";
+export {
+  compareTopologicalFingerprints,
+  createTopologicalFingerprint,
+  validateTopologicalFingerprint,
+} from "./fingerprint.js";
+export {
+  synthesizeCertified,
+  synthesizeTermCertified,
+  validateCertifiedSynthesisAgainstTerm,
+  validateCertifiedSynthesisResult,
+} from "./synthesis.js";
