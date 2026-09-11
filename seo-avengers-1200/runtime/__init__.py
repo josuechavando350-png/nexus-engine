01875b3013b3b495550ad534e99c759c278b5570
josuechavando350-png <josuechavando350@gmail.com>
2026-09-10 23:52:37 -0600

    feat(seo-avengers-1200): expose bridged service entrypoint

--- seo-avengers-1200/runtime/__init__.py
+++ seo-avengers-1200/runtime/__init__.py
@@ -1,3 +1,4 @@
 from .seo_avengers_1200 import SeoAvengers1200Runtime, module_registry
+from .service import execute_avengers_1200
 
-__all__ = ["SeoAvengers1200Runtime", "module_registry"]
+__all__ = ["SeoAvengers1200Runtime", "module_registry", "execute_avengers_1200"]
