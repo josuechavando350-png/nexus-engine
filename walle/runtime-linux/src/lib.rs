844e833e78702070e1f33bffcf818309f68f0584
josuechavando350-png <josuechavando350@gmail.com>
2026-09-12 19:34:41 -0600

    style(walle): rustfmt Linux runtime module root

--- walle/runtime-linux/src/lib.rs
+++ walle/runtime-linux/src/lib.rs
@@ -1,4 +1,7 @@
-#![cfg_attr(not(all(target_os = "linux", target_arch = "x86_64")), allow(dead_code))]
+#![cfg_attr(
+    not(all(target_os = "linux", target_arch = "x86_64")),
+    allow(dead_code)
+)]
 
 #[cfg(not(all(target_os = "linux", target_arch = "x86_64")))]
 compile_error!("walle-runtime-linux currently supports Linux x86_64 only");
