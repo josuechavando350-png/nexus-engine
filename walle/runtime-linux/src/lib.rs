85d06ca0d52cfe29ed15d83aeb591b1ee988eebb
josuechavando350-png <josuechavando350@gmail.com>
2026-09-12 19:27:49 -0600

    feat(walle): expose Linux runtime primitives

---
+++ walle/runtime-linux/src/lib.rs
@@ -0,0 +1,8 @@
+#![cfg_attr(not(all(target_os = "linux", target_arch = "x86_64")), allow(dead_code))]
+
+#[cfg(not(all(target_os = "linux", target_arch = "x86_64")))]
+compile_error!("walle-runtime-linux currently supports Linux x86_64 only");
+
+pub mod artifact;
+pub mod cgroup;
+pub mod firecracker;
